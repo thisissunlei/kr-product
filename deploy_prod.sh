50be6d6311a0f978f9f8f9887c7d3b0231cc35f6
yihaoliu <liuyihao@krspace.cn>
2018-08-06 15:38:37 +0800

    style: 样式修改

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -5,15 +5,15 @@ npm run build
 
 if [ $1 = 'prod01' ]; then
 
-rsync -rvltOD ./dist/* ali-krspace-web-01:/data/work/frontend/kr-project/
+rsync -rvltOD ./dist/* ali-krspace-web-01:/data/work/frontend/kr-product/
 
 elif [ $1 = 'prod02' ]; then
 
-rsync -rvltOD ./dist/* ali-krspace-web-02:/data/work/frontend/kr-project/
+rsync -rvltOD ./dist/* ali-krspace-web-02:/data/work/frontend/kr-product/
 
 elif [ $1 = 'all' ]; then
 
-rsync -rvltOD ./dist/* ali-krspace-web-01:/data/work/frontend/kr-project/
-rsync -rvltOD ./dist/* ali-krspace-web-02:/data/work/frontend/kr-project/
+rsync -rvltOD ./dist/* ali-krspace-web-01:/data/work/frontend/kr-product/
+rsync -rvltOD ./dist/* ali-krspace-web-02:/data/work/frontend/kr-product/
 
 fi
