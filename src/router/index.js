a0486802f1be35b07ed92457040f0d31388fb7c6
wusuling <wusuling@krspace.cn>
2018-08-06 17:19:21 +0800

    feat: 删除多余添加社区

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ Vue.use(Router)
 const GoodsLibrary = () => import('pages/inventory/goods-library')
 const DailyInventory = () => import('pages/inventory/daily-inventory')
 const OptionalInventory = () => import('pages/inventory/optional-inventory')
+const FloorPlan = () => import('pages/inventory/floor-plan')
+const AttractInvestment = () => import('pages/community/attract-investment')
 export default new Router({
   routes: [
     {
@@ -22,5 +24,15 @@ export default new Router({
       name: 'OptionalInventory',
       component: OptionalInventory
     },
+    {
+      path: '/inventory/floor-plan',
+      name: 'FloorPlan',
+      component: FloorPlan
+    },
+    {
+      path: '/community/attract-investment',
+      name: 'AttractInvestment',
+      component: AttractInvestment
+    }
   ]
 })
