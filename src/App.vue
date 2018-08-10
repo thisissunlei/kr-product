1b72ad3c7da1a4d0497c1a86ba1d73178b6efa2d
yihaoliu <liuyihao@krspace.cn>
2018-08-10 18:20:58 +0800

    style：想先显示

--- src/App.vue
+++ src/App.vue
@@ -66,14 +66,14 @@ export default {
     margin: 0;
     padding: 0;
   }
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  color: #2c3e50;
-}
-   .layout{
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /* text-align: center; */
+    color: #2c3e50;
+  }
+  .layout{
       min-height:100%;
       overflow: hidden;
       background: #f5f7f9;
@@ -130,7 +130,6 @@ export default {
   .layout-hide-text .layout-text{
       display: none;
   }
-
   .ivu-col{
       transition: width .2s ease-in-out;
   }
