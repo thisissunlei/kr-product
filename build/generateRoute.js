7bd3aa883e820ec586ef5ac26aa22fd9f29b81e8
wusuling <wusuling@krspace.cn>
2018-08-14 15:59:24 +0800

    feat: route

---
+++ build/generateRoute.js
@@ -0,0 +1,69 @@
+var fs = require('fs')
+  
+  
+//遍历文件夹，获取所有文件夹里面的文件信息
+/*
+ * @param path 路径
+ *
+ */
+
+function geFileList(path)
+{
+ var filesList = [];
+ readFile(path,filesList);
+ return filesList;
+}
+  
+//遍历读取文件
+function readFile(path,filesList)
+{
+ files = fs.readdirSync(path);//需要用到同步读取
+ files.forEach(walk);
+ function walk(file)
+ { 
+  states = fs.statSync(path+'/'+file);   
+  if(states.isDirectory())
+  {
+   readFile(path+'/'+file,filesList);
+  }
+  else
+  { 
+   //创建一个对象保存信息
+   /*var obj = new Object();
+   obj.size = states.size;//文件大小，以字节为单位
+   obj.name = file;//文件名
+   obj.path = path+'/'+file; //文件绝对路径*/
+   var paths=path+'/'+file;
+   filesList.push(paths);
+  }  
+ }
+}
+  
+//写入文件utf-8格式
+function writeFile(fileName,data){ 
+ fs.writeFile(fileName,data,complete);
+ function complete(){
+  console.log("文件生成成功");
+ } 
+}
+  
+var filesList = geFileList("../src/pages");
+var allObj={};
+var newRou=[];
+var portStr='';
+for(var i=0;i<filesList.length;i++){
+    if(filesList[i].indexOf('index.vue')!=-1){
+        var item=filesList[i].split('pages')[1];
+        var switchItem=item.split('/index.vue')[0].replace(/\//g,'R').replace(/-/g,'R');
+        var routeItem=item.split('/index.vue')[0];
+        var routeObj={path:routeItem,name:switchItem,component:switchItem};
+        var port='const '+switchItem+' = () => import("pages'+item+'")';
+        portStr+=port+"\n";
+        newRou.push(routeObj);
+    }
+}
+
+var strRou=JSON.stringify(newRou);
+var strPort=JSON.stringify(portStr);
+strs='module.exports={routes:'+strRou+',port:'+strPort+'}';
+writeFile("../src/router/newRouter.js",strs);
--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,72 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
+import newRoute from './newRouter.js';
 Vue.use(Router)
 
-const GoodsLibrary = () => import('pages/inventory/goods-library/index.vue')
-const DailyInventory = () => import('pages/inventory/daily-inventory/index.vue')
-const OptionalInventory = () => import('pages/inventory/optional-inventory/index.vue')
-const FloorPlan = () => import('pages/inventory/floor-plan/index.vue')
-const AttractInvestment = () => import('pages/community/attract-investment/index.vue')
-const Community = () => import('pages/operation/community/index.vue')
-const VisitorsRecord = () => import('pages/operation/visitors-to-record/index.vue')
-export default new Router({
-  // mode: 'history',
-  routes: [
-    {
-      path: '/inventory/goods-library',
-      name: 'GoodsLibrary',
-      component: GoodsLibrary
-    },
-    {
-      path: '/inventory/daily-inventory',
-      name: 'DailyInventory',
-      component: DailyInventory
-    },
-    {
-      path: '/inventory/optional-inventory',
-      name: 'OptionalInventory',
-      component: OptionalInventory
-    },
-    {
-      path: '/inventory/floor-plan',
-      name: 'FloorPlan',
-      component: FloorPlan
-    },
-    {
-      path: '/community/attract-investment',
-      name: 'AttractInvestment',
-      component: AttractInvestment
-    },
-    {
-      path: '/operation/community',
-      name: 'Community',
-      component: Community
-    },
-    {
-      path: '/operation/visitors-to-record',
-      name: 'VisitorsRecord',
-      component: VisitorsRecord
-    }
-  ]
-})
+console.log('dd',typeof newRoute.port,newRoute.port);
+var portArr=newRoute.port.split('\n');
+console.log('dd',portArr);
+if(portArr&&portArr.length){
+  portArr.map((item,index)=>{
+      console.log('svf',item);
+  })
+}
+newRoute.port;
+if(newRoute.routes&&newRoute.routes.length){
+  newRoute.routes.map((item,index)=>{
+      item.component=eval(item.component);
+  })
+}
+export default new Router({routes:newRoute.routes})
+
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// Vue.use(Router)
+
+// const GoodsLibrary = () => import('pages/inventory/goods-library/index.vue')
+// const DailyInventory = () => import('pages/inventory/daily-inventory/index.vue')
+// const OptionalInventory = () => import('pages/inventory/optional-inventory/index.vue')
+// const FloorPlan = () => import('pages/inventory/floor-plan/index.vue')
+// const AttractInvestment = () => import('pages/community/attract-investment/index.vue')
+// const Community = () => import('pages/operation/community/index.vue')
+// const VisitorsRecord = () => import('pages/operation/visitors-to-record/index.vue')
+// export default new Router({
+//   // mode: 'history',
+//   routes: [
+//     {
+//       path: '/inventory/goods-library',
+//       name: 'GoodsLibrary',
+//       component: GoodsLibrary
+//     },
+//     {
+//       path: '/inventory/daily-inventory',
+//       name: 'DailyInventory',
+//       component: DailyInventory
+//     },
+//     {
+//       path: '/inventory/optional-inventory',
+//       name: 'OptionalInventory',
+//       component: OptionalInventory
+//     },
+//     {
+//       path: '/inventory/floor-plan',
+//       name: 'FloorPlan',
+//       component: FloorPlan
+//     },
+//     {
+//       path: '/community/attract-investment',
+//       name: 'AttractInvestment',
+//       component: AttractInvestment
+//     },
+//     {
+//       path: '/operation/community',
+//       name: 'Community',
+//       component: Community
+//     },
+//     {
+//       path: '/operation/visitors-to-record',
+//       name: 'VisitorsRecord',
+//       component: VisitorsRecord
+//     }
+//   ]
+// })
